4069d19653cf77979d07a0a89e1a05772030100d
Rohit Singh <imrohitsingh4105@gmail.com>
2026-06-26 14:59:37 +0530

    Add check if board is full

--- main.py
+++ main.py
@@ -5,15 +5,28 @@ def game_board(board):
     print(f" {board[3]} | {board[4]} | {board[5]}")
     print("---|---|---")
     print(f" {board[6]} | {board[7]} | {board[8]}")
-    
-def check_for_winner(board,player):
+
+
+def check_for_winner(board, player):
     win_conditions = [
-        [0,1,2],[3,4,5],[6,7,8],
-        [0,3,6],[1,4,7],[2,5,8],
-        [0,4,8],[2,4,6]
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
     ]
     for condition in win_conditions:
         if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
             return True
     return False
+
+
+def is_full(board):
+    for space in board:
+        if space in ['1','2','3','4','5','6','7','8','9']:
+            return False
+    return True
 
