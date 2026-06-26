df6fb3b3398bd429ed6532acb07b0f8ffd63c16e
Rohit Singh <imrohitsingh4105@gmail.com>
2026-06-26 14:49:46 +0530

    Add board and win check function

---
+++ main.py
@@ -0,0 +1,19 @@
+def game_board(board):
+    print("\n")
+    print(f" {board[0]} | {board[1]} | {board[2]}")
+    print("---|---|---")
+    print(f" {board[3]} | {board[4]} | {board[5]}")
+    print("---|---|---")
+    print(f" {board[6]} | {board[7]} | {board[8]}")
+    
+def check_for_winner(board,player):
+    win_conditions = [
+        [0,1,2],[3,4,5],[6,7,8],
+        [0,3,6],[1,4,7],[2,5,8],
+        [0,4,8],[2,4,6]
+    ]
+    for condition in win_conditions:
+        if board[condition[0]] == board[condition[1]] == board[condition[2]] == player:
+            return True
+    return False
+
